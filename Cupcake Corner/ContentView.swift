c2e6549019e4ffbb8e7051c6fc8340454b009391
Marko Živanović <zivanovic.marko@icloud.com>
2025-07-17 15:04:09 +0200

    Create CupcakeCorner Project

---
+++ Cupcake Corner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Cupcake Corner
+//
+//  Created by Marko Zivanovic on 17. 7. 2025..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Cupcake Corner/Cupcake_CornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cupcake_CornerApp.swift
+//  Cupcake Corner
+//
+//  Created by Marko Zivanovic on 17. 7. 2025..
+//
+
+import SwiftUI
+
+@main
+struct Cupcake_CornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
